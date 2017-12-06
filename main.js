8653494df3e187619dc19c6a060d22f972bc1f45
hnameer <wisam105@gmail.com>
2017-12-06 15:49:07 -0600

    hiding credentials

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var main = (bucketName, clientId, clientSecret, fileName, filePath)=>{
     console.log(upload.distUpload());
 }
 
-main('grit_hackathonbua1koq2jxcz66qkowgczeaokwpoydb2',
- 'bUA1koQ2jXcz66qKOWGCzEAOKWPOyDb2',
-  'M510e35bf95184af',
+main('bucketKey',
+ 'clientId',
+  'clientSecret',
   'UrbanHouse-2015.rvt',
   './models/UrbanHouse-2015.rvt'
 
